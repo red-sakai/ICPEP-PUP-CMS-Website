fe88a7138c89675bd3518d39c1c2d61a2c63da5c
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-09-17 00:35:16 +0800

    feat: add Adviser component with Card3 and description section

--- src/components/sections/Adviser.tsx
+++ src/components/sections/Adviser.tsx
@@ -1,11 +1,55 @@
-{/* you can start by removing the p tag */}
+import Card3 from '../ui/Card3';
+
+const adviserName = 'Sir Mahaguay';
+const adviserPosition = 'ICPEP SE PUP ADVISER';
+
+const adviserDescription = `
+Engr. Rolito L. Mahaguay, MSE-Computer, is an Associate Professor I and the Chairperson of the Department of Computer 
+Engineering at the Polytechnic University of the Philippines – College of Engineering, where he drives initiatives to improve 
+curriculum, laboratory resources, and student development. Beyond his role in the department, he also serves as the 
+Faculty Adviser of ICpEP.SE – PUP Chapter, the official student organization for computer engineering, where he actively 
+guides and inspires future engineers to uphold excellence, innovation, and camaraderie.
+`;
 
 const Adviser = () => {
   return (
-    <p>placeholder so there will be no error</p> 
-  )
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: '2.5rem',
+      }}
+    >
+      {/* Card with name, position, photo, and social media */}
+      <Card3
+        name = "Sir Mahaguay"
+        position = "ICPEP SE PUP ADVISER"
+        // photo = "images/adviser.svg"
+        width="450px"
+        photoSize="150px"
+        facebookUrl="https://facebook.com" // example para sa clickable na icon (replace the link nalang po)
+        // twitterUrl="https://twitter.com"
+        // instagramUrl="https://instagram.com"
+        // linkedinUrl="https://linkedin.com"
+      />
 
-  
+      {/* Description */}
+      <div
+        style={{
+          maxWidth: '1500px',
+          marginTop: '2rem',
+          textAlign: 'center',
+          fontSize: '1.3rem',
+          lineHeight: '1.5',
+          color: '#777',
+        }}
+      >
+        {adviserDescription}
+      </div>
+    </div>
+  );
 };
 
 export default Adviser;
