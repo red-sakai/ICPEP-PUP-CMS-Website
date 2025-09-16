0ab69ef15c8bac73c66829023d26ccec9de11f62
Marjoy M. Caranto <marjoycaranto29@gmail.com>
2025-09-17 05:52:49 +0800

    refactor(officers): replace repeated member cards with MembersRow component

--- src/components/sections/Officers.tsx
+++ src/components/sections/Officers.tsx
@@ -1,6 +1,6 @@
 import Card3 from '../ui/Card3';
 
-const executiveMembers = [
+const currentPresident = [
   {
     name: 'Gladwin Ferdz Del Rosario',
     position: 'President',
@@ -9,11 +9,14 @@ const executiveMembers = [
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
     linkedinUrl: "https://linkedin.com"
-  },
+  }
+]
+
+const executiveMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -22,7 +25,7 @@ const executiveMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -31,7 +34,7 @@ const executiveMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -43,7 +46,7 @@ const committeeMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -52,7 +55,7 @@ const committeeMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -61,7 +64,7 @@ const committeeMembers = [
   {
     name: 'Name',
     position: 'Position',
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -73,7 +76,7 @@ const pastPresidents = [
   {
     name: 'Marvin Erosa',
     position: "'24-'25 pres.",
-    // photoUrl: "images/president.svg",
+    // photoUrl: "images/member.svg",
     facebookUrl: "https://facebook.com",
     xUrl: "https://x.com",
     instagramUrl: "https://instagram.com",
@@ -94,6 +97,31 @@ const SectionHeader = ({ title }: { title: string }) => (
     {title}
   </h4>
 );
+
+const MembersRow = ({ members }: { members: typeof executiveMembers }) => {
+  const rows = [];
+  for (let i = 0; i < members.length; i += 3) {
+    const chunk = members.slice(i, i + 3);
+    rows.push(
+      <div key={i} style={{ display: 'flex', justifyContent: 'center', gap: '2rem', flexWrap: 'wrap', marginTop: '2rem' }}>
+        {chunk.map((member, index) => (
+          <Card3
+            key={index}
+            name={member.name}
+            position={member.position}
+            width="400px"
+            photoSize="150px"
+            facebookUrl={member.facebookUrl}
+            xUrl={member.xUrl}
+            instagramUrl={member.instagramUrl}
+            linkedinUrl={member.linkedinUrl}
+          />
+        ))}
+      </div>
+    );
+  }
+  return <>{rows}</>;
+};
 
 const Officers = () => {
   return (
@@ -139,145 +167,32 @@ const Officers = () => {
       >
         <SectionHeader title="Executive Members" />
         {/* President */}
-        <div>
+        <div style={{ marginTop: '2rem' }}>
           <Card3
-            name = {executiveMembers[0].name}
-            position = {executiveMembers[0].position}
-            // photo = {executiveMembers[0].photoUrl}
-            width="650px"
-            photoSize="150px"
-            facebookUrl= {executiveMembers[0].facebookUrl}
-            xUrl= {executiveMembers[0].xUrl}
-            instagramUrl= {executiveMembers[0].instagramUrl}
-            linkedinUrl= {executiveMembers[0].linkedinUrl}
+          name={currentPresident[0].name}
+          position={currentPresident[0].position}
+          width="650px"
+          photoSize="150px"
+          facebookUrl={currentPresident[0].facebookUrl}
+          xUrl={currentPresident[0].xUrl}
+          instagramUrl={currentPresident[0].instagramUrl}
+          linkedinUrl={currentPresident[0].linkedinUrl}
           />
         </div>
         {/* Other Executive Members */}
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '2rem', 
-            flexWrap: 'wrap',
-            marginTop: '2rem',
-          }}
-        >
-          <Card3
-            name={executiveMembers[1].name}
-            position={executiveMembers[1].position}
-            // photoUrl={executiveMembers[1].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={executiveMembers[1].facebookUrl}
-            xUrl={executiveMembers[1].xUrl}
-            instagramUrl={executiveMembers[1].instagramUrl}
-            linkedinUrl={executiveMembers[1].linkedinUrl}
-          />
-          <Card3
-            name={executiveMembers[2].name}
-            position={executiveMembers[2].position}
-            // photoUrl={executiveMembers[2].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={executiveMembers[2].facebookUrl}
-            xUrl={executiveMembers[2].xUrl}
-            instagramUrl={executiveMembers[2].instagramUrl}
-            linkedinUrl={executiveMembers[2].linkedinUrl}
-          />
-          <Card3
-            name={executiveMembers[3].name}
-            position={executiveMembers[3].position}
-            // photoUrl={executiveMembers[3].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={executiveMembers[3].facebookUrl}
-            xUrl={executiveMembers[3].xUrl}
-            instagramUrl={executiveMembers[3].instagramUrl}
-            linkedinUrl={executiveMembers[3].linkedinUrl}
-          />
-        </div>
+        <MembersRow members={executiveMembers} />
       </div>
 
       {/* Committee Members */}
-      <div
-        style={{
-          marginTop: '3rem'
-        }}
-      >
+      <div style={{ marginTop: '3rem' }}>
         <SectionHeader title="Committee Members" />
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '2rem',
-            flexWrap: 'wrap',
-            marginTop: '2rem',
-          }}
-        >
-          <Card3
-            name={committeeMembers[0].name}
-            position={committeeMembers[0].position}
-            // photoUrl={committeeMembers[0].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={committeeMembers[0].facebookUrl}
-            xUrl={committeeMembers[0].xUrl}
-            instagramUrl={committeeMembers[0].instagramUrl}
-            linkedinUrl={committeeMembers[0].linkedinUrl}
-          />
-          <Card3
-            name={committeeMembers[1].name}
-            position={committeeMembers[1].position}
-            // photoUrl={committeeMembers[1].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={committeeMembers[1].facebookUrl}
-            xUrl={committeeMembers[1].xUrl}
-            instagramUrl={committeeMembers[1].instagramUrl}
-            linkedinUrl={committeeMembers[1].linkedinUrl}
-          />
-          <Card3
-            name={committeeMembers[2].name}
-            position={committeeMembers[2].position}
-            // photoUrl={committeeMembers[2].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={committeeMembers[2].facebookUrl}
-            xUrl={committeeMembers[2].xUrl}
-            instagramUrl={committeeMembers[2].instagramUrl}
-            linkedinUrl={committeeMembers[2].linkedinUrl}
-          />
-        </div>
+        <MembersRow members={committeeMembers} />
       </div>
       
       {/* Past Presidents */}
-      <div
-        style={{
-          marginTop: '3rem'
-        }}
-      >
+      <div style={{ marginTop: '3rem' }}>
         <SectionHeader title="Past Presidents" />
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '2rem',
-            flexWrap: 'wrap',
-            marginTop: '2rem',
-          }}
-        >
-          <Card3
-            name={pastPresidents[0].name}
-            position={pastPresidents[0].position}
-            // photoUrl={pastPresidents[0].photoUrl}
-            width="400px"
-            photoSize="150px"
-            facebookUrl={pastPresidents[0].facebookUrl}
-            xUrl={pastPresidents[0].xUrl}
-            instagramUrl={pastPresidents[0].instagramUrl}
-            linkedinUrl={pastPresidents[0].linkedinUrl}
-          />
-        </div>
+        <MembersRow members={pastPresidents} />
       </div>
     </div>
   );
